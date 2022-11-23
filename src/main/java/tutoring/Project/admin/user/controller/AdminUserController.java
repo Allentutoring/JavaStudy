a2f9ffc7245afabb00cc33dfddeb38f643af3038
Allentutoring <allen@tutoring.co.kr>
2022-11-23 09:33:49 +0900

    add custom PreAuthorize annotation

--- src/main/java/tutoring/Project/admin/user/controller/AdminUserController.java
+++ src/main/java/tutoring/Project/admin/user/controller/AdminUserController.java
@@ -3,11 +3,11 @@ package tutoring.Project.admin.user.controller;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tutoring.Project.admin.base.controller.AdminController;
+import tutoring.Project.admin.user.role.IsAdmin;
 import tutoring.Project.auth.entity.User;
 import tutoring.Project.auth.repository.UserRepository;
 
@@ -15,9 +15,10 @@ import tutoring.Project.auth.repository.UserRepository;
 @RequestMapping("/admin/user")
 @AllArgsConstructor
 public class AdminUserController extends AdminController {
+
     private final UserRepository repository;
 
-    @Secured("ROLE_ADMIN")
+    @IsAdmin
     @GetMapping("/all")
     public ResponseEntity<List<User>> allInfo() {
         return ResponseEntity.ok(repository.findAll());
